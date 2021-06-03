308d308d60846c6060dbfcdff0e6fdd61aa2cd0f
Lubna-Hajhussein <lubnahaj45@gmail.com>
2021-06-03 11:27:56 +0300

    initializing the front end files

--- serverApp/app.js
+++ serverApp/app.js
@@ -1,8 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.static("../clientApp"));
 
 app.listen(PORT, () => {
 	console.log(`server is listening to http requests on port ${PORT}`);
